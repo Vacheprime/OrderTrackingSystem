f7a979cc02bbd4509a81e15ecb8cf7bdd311a625
Vacheprime <danatmuradov500@gmail.com>
2025-04-13 17:03:57 -0400

    Made the Client class extend from the Person class.

--- app/models/client.php
+++ app/models/client.php
@@ -17,64 +17,27 @@ use Doctrine\ORM\Mapping\OneToOne;
 use InvalidArgumentException;
 
 require_once(dirname(__DIR__)."/core/utils/utils.php");
+require_once("person.php");
 require_once("address.php");
 
 #[Entity]
 #[Table(name: "client")]
-class Client {
+class Client extends Person {
 
     #[Id]
     #[Column(name: "client_id", type:Types::INTEGER), GeneratedValue("AUTO")]
     private ?int $clientId = null;
 
-    #[Column(name: "first_name", type:Types::STRING)]
-    private string $firstName;
-
-    #[Column(name: "last_name", type:Types::STRING)]
-    private string $lastName;
-
     #[Column(name: "client_reference", type:Types::STRING)]
     private ?string $clientReference;
 
-    #[Column(name: "phone_number", type:Types::STRING)]
-    private string $phoneNumber;
-
-    #[OneToOne(targetEntity: Address::class)]
-    #[JoinColumn(name: "address_id", referencedColumnName: "address_id")]
-    private Address $address;
-
     public function __construct(string $firstName, string $lastName, string $phoneNumber, Address $address, ?string $reference = null) {
-        $this->setFirstName($firstName);
-        $this->setLastName($lastName);
-        $this->setPhoneNumber($phoneNumber);
+        parent::__construct($firstName, $lastName, $phoneNumber, $address);
         $this->setClientReference($reference);
-        $this->address = $address;
     }
 
     public function getClientId(): int {
         return $this->clientId;
-    }
-
-    public function getFirstName(): string {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): void {
-        if (!Utils::validateName($firstName)) {
-            throw new InvalidArgumentException("The first name is invalid!");
-        }
-        $this->firstName = $firstName;
-    }
-
-    public function getLastName(): string {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): void {
-        if (!Utils::validateName($lastName)) {
-            throw new InvalidArgumentException("The last name is invalid!");
-        }
-        $this->lastName = $lastName;
     }
 
     public function getClientReference(): string {
@@ -90,21 +53,6 @@ class Client {
             throw new InvalidArgumentException("The reference is invalid!");
         }
         $this->clientReference = $reference;
-    }
-
-    public function getPhoneNumber(): string {
-        return $this->phoneNumber;
-    }
-
-    public function setPhoneNumber(string $phoneNumber): void {
-        if (!Utils::validatePhoneNumber($phoneNumber)) {
-            throw new InvalidArgumentException("The phone number is invalid!");
-        }
-        $this->phoneNumber = $phoneNumber;
-    }
-
-    public function getAddress(): Address {
-        return $this->address;
     }
 
     /**
