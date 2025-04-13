89e7b778f159f5a9d21efb46171b911719d8b2c4
Vacheprime <danatmuradov500@gmail.com>
2025-04-13 16:15:16 -0400

    Changed address_id field type to nullable int.

--- app/models/address.php
+++ app/models/address.php
@@ -8,21 +8,20 @@ use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Id;
 use Doctrine\ORM\Mapping\Column;
-
-use app\core\utils\Utils;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping\GeneratedValue;
+
+use app\core\utils\Utils;
 use InvalidArgumentException;
 
 require_once(dirname(__DIR__)."/core/utils/utils.php");
-require_once(dirname(__DIR__)."/mysql/orm_config/doctrine_config.php");
 
 #[Entity]
 #[Table("address")]
 class Address {
     #[Id]
     #[Column(name: "address_id", type: Types::INTEGER), GeneratedValue("AUTO")]
-    private int $addressId;
+    private ?int $addressId = null;
 
     #[Column(name: "street_name", type: Types::STRING)]
     private string $streetName;
