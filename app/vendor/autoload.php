2e7fd1fde61c087f698dac31f2b4e2bbb0468221
lydia230 <%email%>
2025-04-15 17:32:23 -0400

    Added Laravel and moved the previous MVC

--- app/vendor/autoload.php
+++ app/vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit6f85502b52ffab0d6a85b356c58b065f::getLoader();
+return ComposerAutoloaderInit88970a0117c062eed55fa8728fc43833::getLoader();
