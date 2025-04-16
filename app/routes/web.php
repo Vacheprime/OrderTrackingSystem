9aa51fe12f0eab74c28d99cfbbba6301f8323565
lydia230 <%email%>
2025-04-16 01:18:32 -0400

    Created basic routing for each controller

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AddressController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderProductController;
+use App\Http\Controllers\PaymentController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// ### Address Routing ###
+Route::resource('addresses', AddressController::class);
+
+
+// ### Client Routing ###
+Route::resource('clients', ClientController::class);
+
+
+// ### Employee Routing ###
+Route::resource('employees', EmployeeController::class);
+
+
+// ### Order Routing ###
+Route::resource('orders', OrderController::class);
+
+
+// ### OrderProduct Routing ###
+Route::resource('products', OrderProductController::class);
+
+
+// ### Payment Routing ###
+Route::resource('payments', PaymentController::class);
+
