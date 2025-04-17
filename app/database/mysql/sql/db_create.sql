379a7f599057054ff45e67af26af42f3f52afdf7
Vacheprime <danatmuradov500@gmail.com>
2025-04-16 21:50:00 -0400

    Changed and corrected the database schema.

--- app/database/mysql/sql/db_create.sql
+++ app/database/mysql/sql/db_create.sql
@@ -19,7 +19,7 @@ CREATE TABLE `client` (
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	client_reference VARCHAR(100),
-	phone_number VARCHAR(20) NOT NULL,
+	phone_number VARCHAR(25) NOT NULL,
 	address_id INTEGER NOT NULL,
 	CONSTRAINT client_address_fk FOREIGN KEY (address_id) REFERENCES address(address_id)
 );
@@ -31,13 +31,12 @@ CREATE TABLE employee (
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	position VARCHAR(25) NOT NULL,
-	phone_number VARCHAR(20) NOT NULL,
+	phone_number VARCHAR(25) NOT NULL,
 	email VARCHAR(75) NOT NULL,
-	birth_date DATE NOT NULL,
-	hire_date DATE NOT NULL,
 	is_admin BOOLEAN NOT NULL,
+    has_set_up_2fa BOOLEAN NOT NULL, -- Needed for first time access
 	password_hash VARCHAR(255) NOT NULL,
-	secret VARCHAR(255),
+	secret VARCHAR(255) NOT NULL, 
 	address_id INTEGER NOT NULL,
 	CONSTRAINT employee_address_fk FOREIGN KEY (address_id) REFERENCES address(address_id)
 );
@@ -45,17 +44,13 @@ CREATE TABLE employee (
 -- Create the order table
 CREATE TABLE `order` (
 	order_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	reference_number VARCHAR(100) NOT NULL,
-	is_plan_ready BOOLEAN NOT NULL,
-	is_in_fabrication BOOLEAN NOT NULL,
-	is_completed BOOLEAN NOT NULL,
-	fabrication_start_date DATE,
+	reference_number VARCHAR(100), -- Required, but has to be not null because its generated in PHP.
 	price DECIMAL(10, 2) NOT NULL,
-	taxes DECIMAL(10, 2) NOT NULL,
+    `status` VARCHAR(25) NOT NULL,
+    invoice_number VARCHAR(100), -- Not required because the invoice is not generated from the start.
+    fabrication_start_date DATE,
 	estimated_install_date DATE,
 	order_completed_date DATE,
-	invoice_number VARCHAR(100),
-	`status` VARCHAR(25),
 	`client_id` INTEGER NOT NULL,
 	measured_by INTEGER NOT NULL,
 	CONSTRAINT order_client_fk FOREIGN KEY (client_id) REFERENCES `client`(client_id),
@@ -65,7 +60,7 @@ CREATE TABLE `order` (
 -- Create order product table
 CREATE TABLE order_product (
 	order_id INTEGER PRIMARY KEY,
-	material_name VARCHAR(100),
+	material_name VARCHAR(100), -- Not required because it can be a sink.
 	is_material_available BOOLEAN NOT NULL,
 	slab_height INTEGER,
 	slab_width INTEGER,
