f178e64b63d171b1e05177b8db103df8f19536e5
Vacheprime <danatmuradov500@gmail.com>
2025-04-19 12:14:54 -0400

    Added two tests for the ClientRepository class.

--- app/tests/Unit/ClientRepositoryTest.php
+++ app/tests/Unit/ClientRepositoryTest.php
@@ -7,6 +7,7 @@ use app\Doctrine\ORM\Repository\ClientRepository;
 use PHPUnit\Framework\TestCase;
 use Doctrine\ORM\EntityManager;
 use Tests\DoctrineSetup;
+use Closure;
 
 require_once(dirname(__DIR__) . "/DoctrineSetup.php");
 require_once("app/Doctrine/ORM/Repository/ClientRepository.php");
@@ -16,6 +17,11 @@ class ClientRepositoryTest extends TestCase
     private EntityManager $em;
     private ClientRepository $repository;
 
+    /**
+     * Setup the test.
+     * 
+     * Every test reinitializes the database and creates the entity manager.
+     */
     public function setUp(): void {
         DoctrineSetup::setUpTestDatabase();
         $this->em = DoctrineSetup::initEntityManager();
@@ -23,10 +29,72 @@ class ClientRepositoryTest extends TestCase
     }
 
     /**
-     * @test
+     * Testing the getAllPaginated() method of the ClientRepository class.
+     * 
+     * Here, all client objects should be returned. However, because
+     * comparing client objects can become tedious and complicated (due 
+     * to the associations), only the first names are compared. 
+     * 
+     * If all first names appear in the results, then all rows are
+     * assumed to be fetched.
      */
-    public function test_that_true_is_true(): void
+    public function testGetAllPaginated(): void
     {
-        $this->assertTrue(true);
+        // Expected first names
+        $expectedFirstNames = [
+            "John",
+            "Emily",
+            "Michael",
+            "Sofia",
+            "Liam"
+        ];
+        $paginator = $this->repository->getAllPaginated(2, 1);
+        // Get all the firstnames of the dataset
+        $actualFirstNames = [];
+        foreach($paginator->items() as $client) {
+            $actualFirstNames[] = $client->getFirstName();
+        }
+        // The paginator has to be refetched for each page
+        while ($paginator->hasMorePages()) {
+            $paginator = $this->repository->getAllPaginated(2, $paginator->currentPage() + 1);
+            foreach($paginator->items() as $client) {
+                $actualFirstNames[] = $client->getFirstName();
+            }
+        }
+        // Assert equals with sorted arrays so that all items appear in the same
+        // order if they are equal.
+        $this->assertEqualsCanonicalizing($expectedFirstNames, $actualFirstNames);
+    }
+
+    /**
+     * Testing the getByNamePaginated() method of the ClientRepository class.
+     * 
+     * Here, all clients with their names matching the
+     * search string should be returned. Only first names will
+     * be compared.
+     * 
+     * If the first names match, the client objects are assumed to be
+     * equal.
+     */
+    public function testByNamePaginated() {
+        $searchString = "doe";
+        $expectedFirstNames = ["John"];
+        $clients = self::getAllItemsFromPaginator(10, 1, function($nbrItems, $pageNum) use ($searchString) {
+            return $this->repository->getByNamePaginated($searchString, $nbrItems, $pageNum);
+        });
+        $actualFirstNames = array_map(fn($client) => $client->getFirstName(), $clients);
+        $this->assertEqualsCanonicalizing($expectedFirstNames, $actualFirstNames);
+    }
+
+    private static function getAllItemsFromPaginator(int $nbrItems, int $pageNum, callable $paginatorClosure): array {
+        $paginator = $paginatorClosure($nbrItems, $pageNum);
+        // Get all the firstnames of the dataset
+        $items = [... $paginator->items()];
+        // The paginator has to be refetched for each page
+        while ($paginator->hasMorePages()) {
+            $paginator = $paginatorClosure($nbrItems, $paginator->currentPage() + 1);
+            $items = [...$items, $paginator->items()];
+        }
+        return $items;
     }
 }
