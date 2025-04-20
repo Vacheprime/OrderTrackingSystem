1ca3da6602510794b7b18c84d1b7cb6ff83be4d7
Vacheprime <danatmuradov500@gmail.com>
2025-04-20 00:23:13 -0400

    Developped new approach to repository architecture on the ClientRepository class.

---
+++ app/app/Doctrine/ORM/Repository/BaseRepository.php
@@ -0,0 +1,69 @@
+<?php
+
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use LaravelDoctrine\ORM\Pagination\PaginatesFromParams;
+
+class BaseRepository extends EntityRepository {
+
+    use PaginatesFromParams;
+
+    private QueryBuilder $qb;
+
+    public function __construct(EntityManagerInterface $em, ClassMetadata $metadata, string $alias) {
+        parent::__construct($em, $metadata);
+        $this->qb = $this->createQueryBuilder($alias);
+    }
+
+    /**
+     * Get all results of the current query as
+     * a LengthAwarePaginator.
+     */
+    public function retrievePaginated(int $nbrOfRows, int $pageNumber): LengthAwarePaginator {
+        return $this->paginate($this->qb->getQuery(), $nbrOfRows, $pageNumber);
+    }
+
+    /**
+     * Get all results of the current query as an array
+     * 
+     * @return mixed The result of the underlying query.
+     */
+    public function retrieve(): mixed {
+        return $this->qb->getQuery()->getResult();
+    }
+
+    /**
+     * Apply a given closure to the current query.
+     */
+    public function filter(Closure $filter): self {
+        // Clone the repository
+        $clonedRepo = clone $this;
+        // Apply the filter on the cloned query builder
+        $filter($clonedRepo->qb);
+        // Returned the cloned repository
+        return $clonedRepo;
+    }
+
+    /**
+     * Count the number of results in the current query.
+     * This method makes use of the Doctrine Paginator's count method.
+     */
+    public function countFromCurrentQuery(): int {
+        return (new Paginator($this->qb->getQuery()))->count();
+    }
+
+    public function getQueryBuilder(): QueryBuilder {
+        return clone $this->qb;
+    }
+
+    /**
+     * Clone the QueryBuilder.
+     */
+    public function __clone() {
+        $this->qb = clone $this->qb;
+    }
+}
--- app/app/Doctrine/ORM/Repository/ClientRepository.php
+++ app/app/Doctrine/ORM/Repository/ClientRepository.php
@@ -4,36 +4,39 @@ declare(strict_types = 1);
 
 namespace app\Doctrine\ORM\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use BaseRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use LaravelDoctrine\ORM\Pagination\PaginatesFromParams;
 
-class ClientRepository extends EntityRepository {
+require_once("BaseRepository.php");
+
+class ClientRepository extends BaseRepository {
     private static string $alias = "c";
 
-    use PaginatesFromParams;
+    public function __construct(EntityManagerInterface $em, ClassMetadata $metadata) {
+        parent::__construct($em, $metadata, self::$alias);
+    }
 
     public function getAllPaginated(int $rowsPerPage, int $pageNumber = 1): LengthAwarePaginator {
-        return $this->paginateAll($rowsPerPage, $pageNumber);
+        return parent::paginateAll($rowsPerPage, $pageNumber);
     }
 
     public function getByNamePaginated(string $name, int $rowsPerPage, int $pageNumber = 1): LengthAwarePaginator {
+        return $this->filterByName($name)->retrievePaginated($rowsPerPage, $pageNumber);
+    }
+
+    public function filterByName(string $name): self {
         // Create the SQL search string
         $searchTarget = "%" . strtolower($name) . "%";
-
-        // Get the QueryBuilder and Expr
-        $qb = $this->createQueryBuilder(self::$alias);
-        $expr = $qb->expr();
-
-        // Build the Query
-        // Use LOWER() SQL function to ignore case
-        $qb->where($expr->orX(
-                "LOWER(c.firstName) LIKE :target",
-                "LOWER(c.lastName) LIKE :target"
-            )
-        )->setParameter(":target", $searchTarget);
-        
-        // Return the Paginator
-        return $this->paginate($qb->getQuery(), $rowsPerPage, $pageNumber);
+        return $this->filter(function (QueryBuilder $qb) use ($searchTarget) {
+                $expr = $qb->expr();
+                $qb->where($expr->orX(
+                    "LOWER(c.firstName) LIKE :target",
+                    "LOWER(c.lastName) LIKE :target"
+                ))->setParameter(":target", $searchTarget);
+            }
+        );
     }
 }
