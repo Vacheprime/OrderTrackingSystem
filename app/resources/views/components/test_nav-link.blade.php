f6a23fb767f8d24e6d3d1b2f660b586df7a0a08b
4lex_16 <cirlana16@outlook.com>
2025-04-21 12:11:11 -0400

    Views: Created test folder for any future uses, deleted all current useless tests

--- app/resources/views/components/test_nav-link.blade.php
+++
@@ -1,6 +0,0 @@
-@props(["active" => false, "img" => "", "href" => ""])
-
-<a class="nav-link {{$active ? "nav-link-active" : ""}}" href="{{$href}}">
-    <img src="{{$img}}"/>
-    <p class="title">{{$slot}}</p>
-</a>
