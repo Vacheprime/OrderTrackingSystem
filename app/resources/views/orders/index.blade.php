383969858bb9013b519a8f0467e17d491bb6a7c6
Ibrahimcomputerscience <6257643@edu.vaniercollege.qc.ca>
2025-04-30 21:34:05 -0400

    Issue merging

--- app/resources/views/orders/index.blade.php
+++ app/resources/views/orders/index.blade.php
@@ -8,7 +8,7 @@
         <div id="orders-content" class="main-content">
             <div class="table-header">
                 <form class="search-form" action="" method="POST">
-                    <x-text-input-property labelText="Search" name="search-bar" :isLabel="false"/>
+                    <x-text-input-property labelText="Search" name="search-bar" :isLabel="false" />
 
                     <x-select-input-property labelText="Search By" name="search-by">
                         <option value="order-id" selected>OrderID</option>
@@ -29,65 +29,67 @@
             </div>
             <table class="search-table">
                 <thead>
-                <tr>
-                    <th>OrderID</th>
-                    <th>ClientID</th>
-                    <th>Reference Number</th>
-                    <th>Measured by</th>
-                    <th>Fabrication start date</th>
-                    <th>Status</th>
-                </tr>
+                    <tr>
+                        <th>OrderID</th>
+                        <th>ClientID</th>
+                        <th>Reference Number</th>
+                        <th>Measured by</th>
+                        <th>Fabrication start date</th>
+                        <th>Status</th>
+                    </tr>
                 </thead>
                 <tbody id="orders-tbody">
                     @forelse($orders as $order)
-                        <tr onclick="">
-                            <td>{{$order->getOrderId()}}</td>
-                            <td>{{$order->getClient()->getClientId()}}</td>
-                            <td>{{$order->getReferenceNumber()}}</td>
-                            <td>{{$order->getMeasuredBy()->getInitials()}}</td>
-                            <td>{{$order->getFabricationStartDate() == null ? "null" : $order->getFabricationStartDate()->format("Y -m -d")}}</td>
-                            <td>{{$order->getStatus()->value}}</td>
-                        </tr>
+                    <tr onclick="">
+                        <td>{{$order->getOrderId()}}</td>
+                        <td>{{$order->getClient()->getClientId()}}</td>
+                        <td>{{$order->getReferenceNumber()}}</td>
+                        <td>{{$order->getMeasuredBy()->getInitials()}}</td>
+                        <td>{{$order->getFabricationStartDate() == null ? "null" : $order->getFabricationStartDate()->format("Y -m -d")}}</td>
+                        <td class="status {{ strtolower($order->getStatus()->value) }}">
+                            {{$order->getStatus()->value}}
+                        </td>
+                    </tr>
                     @empty
-                        <tr>
-                            <td>Empty</td>
-                            <td>Empty</td>
-                            <td>Empty</td>
-                            <td>Empty</td>
-                            <td>Empty</td>
-                            <td>Empty</td>
-                        </tr>
+                    <tr>
+                        <td>Empty</td>
+                        <td>Empty</td>
+                        <td>Empty</td>
+                        <td>Empty</td>
+                        <td>Empty</td>
+                        <td>Empty</td>
+                    </tr>
                     @endforelse
                 </tbody>
             </table>
         </div>
         @if(!empty($orders))
-            <div id="orders-side-content" class="side-content">
-                <h2>ORDER DETAILS</h2>
-                <hr>
-                <div class="side-content-scrollable">
-                    <h3><b>ORDER ID:</b><span>#</span></h3>
-                    <p><b>CLIENT ID:</b><span>#</span></p>
-                    <p><b>Measured By:</b><span>#</span></p>
-                    <p><b>Reference Number:</b><span>#</span></p>
-                    <p><b>Invoice Number:</b><span>#</span></p>
-                    <p><b>Total Price:</b><span>#</span></p>
-                    <p><b>Status:</b><span>#</span></p>
-                    <p><b>Fabrication Start Date:</b><span>#</span></p>
-                    <p><b>Installation Start Date:</b><span>#</span></p>
-                    <p><b>Pick Up Date:</b><span>#</span></p>
-                    <p><b>Material Name:</b><span>#</span></p>
-                    <p><b>Slab Height:</b><span>#</span></p>
-                    <p><b>Slab Width:</b><span>#</span></p>
-                    <p><b>Slab Thickness:</b><span>#</span></p>
-                    <p><b>Slab Square Footage:</b><span>#</span></p>
-                    <p><b>Sink Type:</b><span>#</span></p>
-                    <p><b>Fabrication Plan Image:</b><img src="" id="fabPLanImg"/></p>
-                    <p><b>Product Description:</b><textarea placeholder="Product Description"></textarea></p>
-                    <p><b>Product Notes:</b><textarea placeholder="Product Notes"></textarea></p>
-                </div>
-                <a href="/orders/edit"><button class="regular-button" onclick="">Edit</button></a>
+        <div id="orders-side-content" class="side-content">
+            <h2>ORDER DETAILS</h2>
+            <hr>
+            <div class="side-content-scrollable">
+                <h3><b>ORDER ID:</b><span>#</span></h3>
+                <p><b>Client id:</b><span>#</span></p>
+                <p><b>Measured By:</b><span>#</span></p>
+                <p><b>Reference Number:</b><span>#</span></p>
+                <p><b>Invoice Number:</b><span>#</span></p>
+                <p><b>Total Price:</b><span>#</span></p>
+                <p><b>Status:</b><span>#</span></p>
+                <p><b>Fabrication Start Date:</b><span>#</span></p>
+                <p><b>Installation Start Date:</b><span>#</span></p>
+                <p><b>Pick Up Date:</b><span>#</span></p>
+                <p><b>Material Name:</b><span>#</span></p>
+                <p><b>Slab Height:</b><span>#</span></p>
+                <p><b>Slab Width:</b><span>#</span></p>
+                <p><b>Slab Thickness:</b><span>#</span></p>
+                <p><b>Slab Square Footage:</b><span>#</span></p>
+                <p><b>Sink Type:</b><span>#</span></p>
+                <p><b>Fabrication Plan Image:</b><img src="" id="fabPLanImg" /></p>
+                <p><b>Product Description:</b><textarea placeholder="Product Description"></textarea></p>
+                <p><b>Product Notes:</b><textarea placeholder="Product Notes"></textarea></p>
             </div>
+            <a href="/orders/edit"><button class="regular-button" onclick="">Edit</button></a>
+        </div>
         @endif
     </div>
 </x-layout>
