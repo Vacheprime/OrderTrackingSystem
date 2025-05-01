ba6f35ed92d23d4a3ee766175da60d612a4075b1
4lex_16 <cirlana16@outlook.com>
2025-05-01 16:05:18 -0400

    Views: (HTML/CSS) Created Order Tracking Input & Display pages + css

---
+++ app/resources/views/tracking/order-display.blade.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" href="{{ asset('css/ordertrackingdisplay.css') }}">
+
+<x-login-layout title="Client Order Tracking">
+    <div class="main-content">
+        <h1>Order Status</h1>
+        <div id="tracking-status-body">
+            <div id="product-status-div">
+
+            </div>
+            <div id="product-details-div">
+
+            </div>
+        </div>
+    </div>
+</x-login-layout>
---
+++ app/resources/views/tracking/order-input.blade.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="{{ asset('css/ordertracking.css') }}">
+
+<x-login-layout title="Client Order Tracking">
+    <img alt="Crown Granite Logo" src="{{asset("images/logo.png")}}"/>
+    <div class="main-content">
+        <h1>Order Tracking</h1>
+        <div id="tracking-info-div">
+
+        </div>
+        <div id="tracking-reference-div">
+
+        </div>
+    </div>
+</x-login-layout>
