b8656b2d2fa405e1c060ab0ae81229a151646c82
4lex_16 <cirlana16@outlook.com>
2025-05-04 21:53:48 -0400

    Views: (HTML) Added tables to home page

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -1,13 +1,24 @@
 <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+<link rel="stylesheet" href="{{ asset('css/table.css') }}">
+<script src="{{ asset('js/tables.js') }}"></script>
+
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        initializeOrderRowClickEvents();
+        highlightOrderFirstRow();
+    });
+</script>
 
 <x-layout title="Home"> <!-- This Blade component refers to layout.blade.php. It's used to define reusable layout structure-->
     <h1 class="content-title">HOME</h1>
     <div class="content-container">
         <div id="home-content" class="main-content">
-            <h1>Next 7 Weeks</h1>
+            <h1>Next 7 Days</h1>
+            <x-order-table :orders="$orders[0]"/>
         </div>
         <div id="home-side-content" class="main-content">
             <h1>Recently Changed</h1>
+            <x-order-table :orders="$orders[1]"/>
         </div>
     </div>
 </x-layout>
