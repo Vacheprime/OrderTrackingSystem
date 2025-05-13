ff68eeae77ca73bf885298e157532e9ffbeacf82
4lex_16 <cirlana16@outlook.com>
2025-05-13 16:13:08 -0400

    Views: (HTML) Changed the layout

--- app/resources/views/tracking/order-input.blade.php
+++ app/resources/views/tracking/order-input.blade.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="{{ asset('css/trackinginput.css') }}">
 <link rel="stylesheet" href="{{ asset('css/clients.css') }}">
 
-<x-layout title="Client Order Tracking">
+<x-tracking-layout title="Client Order Tracking">
     <div class="layout-container">
     <div id="track-content" class="content-container">
         <div class="main-content">
@@ -9,9 +9,9 @@
             <p>Please enter your reference number to track your order status. If you need help finding it, check your confirmation email or contact our support team.</p>
             <div id="tracking-reference-div">
                 <input type="text" placeholder="Reference Number"/>
-            </div> 
+            </div>
             <a href="/tracking/display"><button class="regular-button">Track Order</button></a>
         </div>
     </div>
     </div>
-</x-layout>
+</x-tracking-layout>
