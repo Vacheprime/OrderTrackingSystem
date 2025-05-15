c00ba5b6747b6f787e9dca723962ea59fd5c119d
4lex_16 <cirlana16@outlook.com>
2025-05-15 11:42:33 -0400

    Main: Added Popup to all indexes

--- app/resources/views/clients/index.blade.php
+++ app/resources/views/clients/index.blade.php
@@ -11,6 +11,9 @@
 
 <x-layout title="Client Management">
     <h1 class="content-title">CLIENT MANAGEMENT</h1>
+    @isset($messageHeader)
+        <p id="{{$messageType}}" class="message-header">{{$messageHeader}}<button onclick="document.getElementById('{{$messageType}}').remove()">x</button></p>
+    @endisset
     <div class="content-container">
         <div id="clients-content" class="main-content">
             <div class="table-content">
--- app/resources/views/payments/index.blade.php
+++ app/resources/views/payments/index.blade.php
@@ -11,6 +11,9 @@
 
 <x-layout title="Payment Management">
     <h1 class="content-title">PAYMENT MANAGEMENT</h1>
+    @isset($messageHeader)
+        <p id="{{$messageType}}" class="message-header">{{$messageHeader}}<button onclick="document.getElementById('{{$messageType}}').remove()">x</button></p>
+    @endisset
     <div class="content-container">
         <div id="payments-content" class="main-content">
             <div class="table-content">
