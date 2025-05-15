9a1ec47c6050be52385a6d1c11e58c58867e4d1c
4lex_16 <cirlana16@outlook.com>
2025-05-15 09:42:14 -0400

    Main: Update web imports

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\TrackingController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
