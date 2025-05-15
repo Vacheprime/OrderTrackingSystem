8d6e392290fdad859e6bab7f5eae0808da53c448
Vacheprime <danatmuradov500@gmail.com>
2025-05-14 23:00:48 -0400

    Models [Payment]: Fixed getOrder() return type.

--- app/app/Doctrine/ORM/Entity/Payment.php
+++ app/app/Doctrine/ORM/Entity/Payment.php
@@ -105,7 +105,7 @@ class Payment {
         $this->paymentDate = $paymentDate;
     }
 
-    public function getOrder(): Order {
+    public function getOrder(): ?Order {
         return $this->order;
     }
 
