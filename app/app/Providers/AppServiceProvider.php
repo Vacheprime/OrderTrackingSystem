0072d9f22fac7372528a73dffe1c8d2603cb77bd
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-05-24 12:43:40 -0400

    Validation: [Service Provider]: Implemented route model binding for the Order class.

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use app\Doctrine\ORM\Entity\Order;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Define model binding for the order model
+        Route::bind("order", function (string $value) {
+            return $this->findOrder($value);
+        });
+    }
+
+    /**
+     * Find an order or return a 404 error page.
+     */
+    private function findOrder(string $value) {
+        // Get the entity manager and the order repository
+        $em = app("em");
+        $orderRepository = $em->getRepository(Order::class);
+
+        // Validate the order id
+        $orderId = filter_var($value, FILTER_VALIDATE_INT);
+        if ($orderId === false || $orderId < 1) {
+            return abort(404, "Order not found.");
+        }
+
+        // fetch the order
+        $order = $orderRepository->find($orderId);
+
+        return $order ?? abort(404, "Order not found.");
     }
 }
