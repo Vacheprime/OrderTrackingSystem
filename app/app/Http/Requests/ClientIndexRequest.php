4454d3bae9d960eb0bda258f5b5327cc1133ee70
Vacheprime <danatmuradov500@gmail.com>
2025-05-24 22:37:55 -0400

    Validation [ClientIndexRequest]: Created form request validation for client index page.

---
+++ app/app/Http/Requests/ClientIndexRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientIndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; // Managed by middleware
+    }
+
+    protected function prepareForValidation(): void {
+        $this->mergeIfMissing([
+            "page" => 1,
+            "search" => "",
+            "searchby" => "client-id",
+            "orderby" => "status",
+            "orderId" => 1
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "page" => "nullable|int|min:1",
+            "search" => "nullable|string",
+            "searchby" => "nullable|string",
+            "orderby" => "nullable|string",
+            "clientId" => "nullable|int|min:1"
+        ];
+    }
+}
