d59fac5bf7e22259fa64c5ded0dbc9bc42c1f2c1
Vacheprime <danatmuradov500@gmail.com>
2025-06-03 14:09:18 -0400

    Views [Employee Create]: Added password and confirm password fields.

--- app/resources/views/components/inputs/text-input-property.blade.php
+++ app/resources/views/components/inputs/text-input-property.blade.php
@@ -5,7 +5,7 @@
         <label id="{{$name}}-label" for="{{$name}}-input">{{$labelText}}</label>
     @endif
     <input {{$readonly ? "readonly" : ""}} type="{{$password ? "password": "text"}}" id="{{$name}}-input"
-           name="{{$name}}" placeholder="{{$labelText}}" value="{{ $value == "" ? old($name) : $value }}" {{ $disabled ? "disabled" : "" }}/>
+           name="{{$name}}" placeholder="{{$labelText}}" @if($value != "" && !$password) value="{{ $value }}" @elseif(!$password) value="{{ old($name) }}" @endif {{ $disabled ? "disabled" : "" }}/>
     
     @error("$name")
         <p class="error-input">{{$message}}</p>
