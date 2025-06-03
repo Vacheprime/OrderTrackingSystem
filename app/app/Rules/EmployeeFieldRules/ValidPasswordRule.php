5d759f3944ec287db95dde239d3f4de308ec84a3
Vacheprime <danatmuradov500@gmail.com>
2025-06-03 14:11:57 -0400

    Validation [ValidPasswordRule]: Created rule for validating passwords.

---
+++ app/app/Rules/EmployeeFieldRules/ValidPasswordRule.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Rules\EmployeeFieldRules;
+
+use App\Rules\BaseValidationRule;
+use app\Utils\Utils;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class ValidPasswordRule extends BaseValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        // Execute preliminary validation
+        if (!$this->executePreliminaryValidation($attribute, $value, $fail)) {
+            return; // fail fast
+        }
+        // Execute secondary validation (Business validation)
+        // Check if the password is valid
+        if (!Utils::validatePassword($value)) {
+            $fail("The password is of invalid format.");
+        }
+    }
+
+    /**
+     * Get the preliminary validation rules.
+     */
+    protected function getValidationRules(string $attribute): array {
+        return [
+            $attribute => "required|string"
+        ];
+    }
+
+    /**
+     * Get the preliminary error messages.
+     */
+    protected function getErrorMessages(string $attribute): array {
+        return [
+            "$attribute.required" => "The password is required.",
+            "$attribute.string" => "The password must be a string.",
+        ];
+    }
+}
