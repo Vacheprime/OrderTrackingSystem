e0b76da95be2d2ee4f438a26a340a6dce37d34e0
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-06-03 19:19:16 -0400

    Views [HTML]: Adjusted HTML for the account form.

--- app/resources/views/user/account.blade.php
+++ app/resources/views/user/account.blade.php
@@ -3,29 +3,30 @@
 <x-layout title="Account">
     <h1 class="content-title">ACCOUNT</h1>
     <div class="content-container">
-    <div id="account-content" class="main-content">
-        <div id="account-details" class="account-div">
-            <div id="account-information">
-                <x-text-input-property labelText="First name" name="fname"/>
-                <x-text-input-property labelText="Last name" name="lname"/>
-                <x-text-input-property labelText="Email" name="email"/>
-                <x-text-input-property labelText="Phone number" name="phonenumber"/>
-{{--                <x-text-input-property labelText="Password" name="password" readonly/>--}}
-            </div>
-            <div id="account-address">
-                <div>
-                    <x-text-input-property labelText="Address" name="address"/>
-                    <x-text-input-property labelText="Postal code" name="postalcode"/>
-                    <x-text-input-property labelText="City" name="city"/>
-                    <x-text-input-property labelText="Province" name="Province"/>
+        <div id="account-content" class="main-content">
+            <div class="account-details">
+                <form id="account-form" method="POST" action="/account" class="create-edit-form">
+                @csrf
+                @method('PUT')
+                <div id="account-information" class="details-div">
+                    <h3>Employee Information</h3>
+                    <x-text-input-property labelText="First name" name="first-name" :value="$employee->getFirstName()"/>
+                    <x-text-input-property labelText="Last name" name="last-name" :value="$employee->getLastName()"/>
+                    <x-text-input-property labelText="Email" name="email" :value="$employee->getAccount()->getEmail()"/>
+                    <x-text-input-property labelText="Phone number" name="phone-number" :value="$employee->getPhoneNumber()"/>
                 </div>
-                <div>
-{{--                    <button class="regular-button">Reset Password</button>--}}
+                <div id="account-address" class="details-div">
+                    <h3>Address Information</h3>
+                    <x-text-input-property labelText="Street" name="street" :value="$employee->getAddress()->getStreetName()"/>
+                    <x-text-input-property labelText="Apartment Number" name="apartment-number" :value="$employee->getAddress()->getAppartmentNumber()"/>
+                    <x-text-input-property labelText="Postal code" name="postal-code" :value="$employee->getAddress()->getPostalCode()"/>
+                    <x-text-input-property labelText="Area" name="area" :value="$employee->getAddress()->getArea()"/>
                 </div>
-            </div>
+                <div class="action-input-div">
+                    <button type="submit" class="regular-button">Apply changes</button>
+                </div>
+            </form>  
+            </div>   
         </div>
-
-        <button class="regular-button">Apply changes</button>
-    </div>
     </div>
 </x-layout>
