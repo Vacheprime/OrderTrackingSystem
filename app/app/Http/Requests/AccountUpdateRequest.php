3843f1acd11ad07ebbeb2db181e1e257518d96e7
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-06-03 22:09:02 -0400

    Validation [AccountUpdateRequest]: Implemented form request validation for user account update.

---
+++ app/app/Http/Requests/AccountUpdateRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\AddressFieldRules\ValidAppartmentNumberRule;
+use App\Rules\AddressFieldRules\ValidAreaRule;
+use App\Rules\AddressFieldRules\ValidPostalCodeRule;
+use App\Rules\AddressFieldRules\ValidStreetRule;
+use App\Rules\EmployeeFieldRules\ValidEmailRule;
+use App\Rules\EmployeeFieldRules\ValidInitialsRule;
+use App\Rules\EmployeeFieldRules\ValidPasswordRule;
+use App\Rules\PersonFieldRules\ValidNameRule;
+use App\Rules\PersonFieldRules\ValidPhoneNumberRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AccountUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true; // Managed by middleware
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "initials" => [new ValidInitialsRule],
+            "first-name" => [new ValidNameRule],
+            "last-name" => [new ValidNameRule],
+            "email" => [new ValidEmailRule],
+            "phone-number" => [new ValidPhoneNumberRule],
+            "street" => [new ValidStreetRule],
+            "apartment-number" => [new ValidAppartmentNumberRule],
+            "postal-code" => [new ValidPostalCodeRule],
+            "area" => [new ValidAreaRule],
+            "password" => [new ValidPasswordRule(false)], // Password is optional for updates
+            "confirm-password" => "same:password",
+        ];
+    }
+}
