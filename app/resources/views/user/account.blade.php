a973571502319e712ddc753052c9d1cbfa955a17
Danat Ali Muradov <danatmuradov500@gmail.com>
2025-06-03 22:16:32 -0400

    Views [Account]: Changed HTML to fix fields and added message notification.

--- app/resources/views/user/account.blade.php
+++ app/resources/views/user/account.blade.php
@@ -1,27 +1,39 @@
 <link rel="stylesheet" href="{{ asset('css/account.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
 <x-layout title="Account">
     <h1 class="content-title">ACCOUNT</h1>
+    @isset($messageHeader)
+        <p id="{{$messageType}}" class="message-header">{{$messageHeader}}<button onclick="document.getElementById('{{$messageType}}').remove()">x</button></p>
+    @endisset
     <div class="content-container">
         <div id="account-content" class="main-content">
             <div class="account-details">
                 <form id="account-form" method="POST" action="/account" class="create-edit-form">
                 @csrf
                 @method('PUT')
-                <div id="account-information" class="details-div">
+                <div id="employee-information" class="details-div">
                     <h3>Employee Information</h3>
+                    <x-text-input-property labelText="Initials" name="initials" :value="$employee->getInitials()"/>
                     <x-text-input-property labelText="First name" name="first-name" :value="$employee->getFirstName()"/>
                     <x-text-input-property labelText="Last name" name="last-name" :value="$employee->getLastName()"/>
-                    <x-text-input-property labelText="Email" name="email" :value="$employee->getAccount()->getEmail()"/>
                     <x-text-input-property labelText="Phone number" name="phone-number" :value="$employee->getPhoneNumber()"/>
                 </div>
-                <div id="account-address" class="details-div">
+                <div id="address-information" class="details-div">
                     <h3>Address Information</h3>
                     <x-text-input-property labelText="Street" name="street" :value="$employee->getAddress()->getStreetName()"/>
                     <x-text-input-property labelText="Apartment Number" name="apartment-number" :value="$employee->getAddress()->getAppartmentNumber()"/>
                     <x-text-input-property labelText="Postal code" name="postal-code" :value="$employee->getAddress()->getPostalCode()"/>
                     <x-text-input-property labelText="Area" name="area" :value="$employee->getAddress()->getArea()"/>
                 </div>
+                <div id="account-information" class="details-div">
+                    <h3>Account Information</h3>
+                    <x-text-input-property labelText="Email" name="email" :value="$employee->getAccount()->getEmail()"/>
+                    <x-text-input-property labelText="New Password" name="password" type="password"/>
+                    <x-text-input-property labelText="Confirm Password" name="confirm-password" type="password"/>
+                </div>
+                <p><i class="fa-solid fa-circle-info"></i> Leave the Password and Confirm Password fields empty if you do not wish to 
+                    update the employee's password.</p>
                 <div class="action-input-div">
                     <button type="submit" class="regular-button">Apply changes</button>
                 </div>
