86b1ab2f29b461a8c1598a4724a00b38a8a6a1a5
Vacheprime <danatmuradov500@gmail.com>
2025-06-04 13:53:45 -0400

    Activity repository: Added an update method and a method to filter by order ID.

--- app/app/Doctrine/ORM/Repository/ActivityRepository.php
+++ app/app/Doctrine/ORM/Repository/ActivityRepository.php
@@ -23,21 +23,6 @@ class ActivityRepository extends BaseRepository {
     }
 
     /**
-     * Filter activities by type.
-     * 
-     * @param ActivityType $type The activity type to filter by.
-     * @return self A clone of the ActivityRepository with the filtering applied.
-     */
-    public function filterByType(ActivityType $type): self {
-        return $this->filter(function (QueryBuilder $qb) use ($type) {
-            $expr = $qb->expr();
-            return $qb->andWhere(
-                $expr->eq("a.activityType", ":type")
-            )->setParameter(":type", $type->value);
-        });
-    }
-
-    /**
      * Inserts a new Activity entity into the database.
      *
      * Persists the provided Activity object if it does not already exist in the database.
@@ -57,6 +42,43 @@ class ActivityRepository extends BaseRepository {
         $em->persist($activity);
         // Apply the changes to the database
         $em->flush();
+    }
+
+    /**
+     * Updates an existing Activity entity in the database.
+     *
+     * Updates the provided Activity object if it already exists in the database.
+     * Throws an InvalidArgumentException if the Activity does not have an ID (i.e., is not persisted).
+     *
+     * @param Activity $activity The Activity entity to update.
+     * @throws InvalidArgumentException If the activity does not exist in the database.
+     * @return void
+     */
+    public function updateActivity(Activity $activity) {
+        if ($activity->getActivityId() === null) {
+            throw new InvalidArgumentException("The activity provided is not in the database.");
+        }
+        // Get the entity manager
+        $em = $this->getEntityManager();
+        // Persist if not done so
+        $em->persist($activity);
+        // Apply the changes to the database
+        $em->flush();
+    }
+
+    /**
+     * Filter activities by type.
+     * 
+     * @param ActivityType $type The activity type to filter by.
+     * @return self A clone of the ActivityRepository with the filtering applied.
+     */
+    public function filterByType(ActivityType $type): self {
+        return $this->filter(function (QueryBuilder $qb) use ($type) {
+            $expr = $qb->expr();
+            return $qb->andWhere(
+                $expr->eq("a.activityType", ":type")
+            )->setParameter(":type", $type->value);
+        });
     }
 
     /**
@@ -90,6 +112,24 @@ class ActivityRepository extends BaseRepository {
                 ->join("a.employee", "e")
                 ->andWhere($expr->eq("e.employeeId", ":employeeId"))
                 ->setParameter(":employeeId", $employeeId);
+            return $qb;
+        });
+    }
+
+    /**
+     * Find only the activities that are related to the specified order.
+     * 
+     * @param int $orderId The Id of the order that the activities are related to.
+     * @return self A clone of the ActivityRepository with the sorting applied.
+     */
+    public function withOrderId(int $orderId): self {
+        return $this->filter(function (QueryBuilder $qb) use ($orderId) {
+            // expr
+            $expr = $qb->expr();
+            $qb = $qb
+                ->join("a.order", "o")
+                ->andWhere($expr->eq("o.orderId", ":orderId"))
+                ->setParameter(":orderId", $orderId);
             return $qb;
         });
     }
