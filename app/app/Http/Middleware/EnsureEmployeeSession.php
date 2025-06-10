bb374f9cf2fc997ca2fe1623f80b9763f0a41fbb
Vacheprime <danatmuradov500@gmail.com>
2025-06-10 13:18:07 -0400

    Middleware: Added checks to verify if the employee has completed 2FA before continuing.

--- app/app/Http/Middleware/EnsureEmployeeSession.php
+++ app/app/Http/Middleware/EnsureEmployeeSession.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class EnsureEmployeeSession
@@ -14,8 +15,9 @@ class EnsureEmployeeSession
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response
-    {
-        if (!session()->has('employee')) {
+    {   
+        $employeeInfo = session()->get('employee');
+        if (!session()->has('employee') || !$employeeInfo["2fa_setup"]) {
             // Send a 401 error response if the content requested is json or is a refresh
             if ($request->expectsJson() || $request->hasHeader("x-refresh-table") || $request->hasHeader("x-change-details")) {
                 return response()->json(['error' => 'Unauthorized', 'redirectTo' => "/"], 401);
